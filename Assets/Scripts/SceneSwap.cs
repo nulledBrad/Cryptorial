fb0ccc0a944e7234e4bd8b6bd3e43d0802f2dfa7
nulledBrad <47425929+nulledBrad@users.noreply.github.com>
2020-12-08 17:01:30 -0500

    UI UPDATE
    -Added HOME BUTTON
    -Added Market Button
    -Added Pattern Button
    -Added Market Title
    -Added Cryptorial Title
    BACKEND STUFF==
    -Added SceneSwap
    -Linked Markets and HomeButtons

---
+++ Assets/Scripts/SceneSwap.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSwap : MonoBehaviour
+{
+    // Start is called before the first frame update
+   public void LoadHome() {
+    
+       SceneManager.LoadScene("MainScene");
+   }
+   public void LoadPatterns() {
+       //TODO
+   }
+   public void LoadMarkets() {
+       SceneManager.LoadScene("Markets");
+   }
+   
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
